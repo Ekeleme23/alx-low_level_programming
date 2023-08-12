17066457eeeb99e5b8673f052dd77d12a54aee55
Ekeleme23 <kelvinnnamdi95@gmail.com>
2023-08-12 09:19:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - A proprogram that prints exactly "Programming is like building a multil
+ingual puzzle, followed by a new line
+*
+*Returns: 0 (success)
+*/
+	int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+	}
