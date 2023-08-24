5d200501d372eae8797a3a516345007d3edbd0f0
Ekeleme23 <kelvinnnamdi95@gmail.com>
2023-08-24 06:43:32 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_puts - Displays values to a string
+*@str: the string to be printed
+*
+*Return nothing
+*/
+
+void _puts(char *str)
+{
+for (; *str != '\0'; *str++)
+{
+_putchar(*str);
+}
+
+_putchar('\n');
+}
