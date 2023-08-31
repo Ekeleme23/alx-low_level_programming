a50d489752c214510a0d75958e45f36db9c68fc9
Ekeleme23 <kelvinnnamdi95@gmail.com>
2023-08-31 16:36:10 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
-/*
-*_puts_recursion - Prints a string followed by a new line
+/**
+* _puts_recursion - Prints a string followed by a new line
+*
 *@s: The variable that holds the string
 *
 *Return: nothing
-*
 */
 void _puts_recursion(char *s)
 {
