e4a1435a0781b8c8aeececce31f5a5e256073984
Ekeleme23 <kelvinnnamdi95@gmail.com>
2023-09-01 05:50:51 -0700

    0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
 *factorial - returns factorial of a given number
 *@n: variable that holds the factorial
 *
-*Return 1 if n is equal to 1 and returns count, and -1
+*Return: 1 if n is equal to 1 and returns count, and -1
 */
 
 int factorial(int n)
