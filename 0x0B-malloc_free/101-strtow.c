5858ce3bd19ba551a0fa699fa84f265524bc1049
Ekeleme23 <Kelvinnnamdi95@gmail.com>
2023-10-30 21:27:14 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -63,9 +63,8 @@ char **strtow(char *str)
 	tmp = (char *) malloc(sizeof(char) * (c + 1));
 	if (tmp == NULL)
 		return (NULL);
-
-		while (start < end)
-		*tmp++ = str[start++];
+	while (start < end)
+	*tmp++ = str[start++];
 	*tmp = '\0';
 	matrix[k] = tmp - c;
 	k++;
