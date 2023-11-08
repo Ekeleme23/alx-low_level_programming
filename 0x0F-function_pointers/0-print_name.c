dbecd071071b548ba8edca2b262a703e62e6b254
Ekeleme23 <Kelvinnnamdi95@gmail.com>
2023-11-08 04:47:59 -0800

    task1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
+
 /* name_printer.c */
 
 #include "function_pointers.h"
 
 /**
- * printName - Print a name
- * @name: The name to be printed
- */
+* printName - Print a name
+* @name: The name to be printed
+*/
 void printName(char *name)
 {
 	/* Your code to print the name */
@@ -13,22 +14,12 @@ void printName(char *name)
 }
 
 /**
- * print_name - Call a function to print a name
- * @name: The name to be printed
- * @f: The function to print the name
- */
+* print_name - Call a function to print a name
+* @name: The name to be printed
+* @f: The function to print the name
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
-}
-
-int main(void)
-{
-	char *name = "John";
-	
-	/* Example usage: */
-	print_name(name, printName);
-
-	return (0);
 }
 
